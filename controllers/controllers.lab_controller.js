bd761a225c000a25b108c34cbb7dcef1e2aae8f8
deku99hh <omar.rashed1000@gmail.com>
2026-05-02 23:36:05 +0300

    fuck AI

--- controllers/controllers.lab_controller.js
+++ controllers/controllers.lab_controller.js
@@ -1,150 +1,287 @@
 
-const PcsData = require('../models/lab_controller.model')
-const shutdownWS = require('./shutdownWS.lab_controller')
+// const PcsData = require('../models/lab_controller.model')
+// const shutdownWS = require('./shutdownWS.lab_controller')
 
+
+// const starting = (req, res) => {
+//     res.send('starting is working')
+// }
+
+// const addToDatabaseClint = async (req, res) => {
+//     const clintdata = req.body
+//     const {pcNumber, labNumber, macAddress, password} = req.body
+//     // res.json(clint)
+//     // res.send('updateDatabaseClint is working')
+//     if (password == 123) {
+
+//         const newPcsData = new PcsData(clintdata.data)
+        
+//         await newPcsData.save()
+//         res.send('done')
+//             // {
+//             //     "password" : 123,   
+//             //     "data" : {
+//             //         "status": {
+//             //             "mouce": "2023-10-25T10:30:00.000Z",
+//             //             "keyboard": "2023-10-25T10:30:00.000Z",
+//             //             "openedTab": ["goos.com"]
+//             //         },
+//             //         "macAddress": "02:1A:2B:dsfsdfdfsdf:4D:5E",
+//             //         "pcNumber": 18,
+//             //         "labNumber": 1,
+//             //         "timeOfBoot": "2023-10-25T08:00:00.000Z",
+//             //         "openTimeAllowed": null
+//             //     }
+//             // }
+//     } else {
+//         res.send('error')
+//     }
+//     // mongodb
+//     // update database with (clint)
+
+//     // await PcsData.updateOne(
+//     //     { pcNumber : pcNumber, labNumber : labNumber, macAddress : macAddress, }, 
+//     //     { $set : clintdata }
+//     // );
+// }
+
+// const updateDatabaseClint = async (req, res) => {
+//     const clintdata = req.body
+//     const {pcNumber, labNumber, macAddress} = req.body
+//     // res.json(clint)
+//     // res.send('updateDatabaseClint is working')
+//     // const newPcsData = new PcsData(clintdata)
+//     // mongodb
+//     // update database with (clint)
+//     // await newPcsData.save()
+//     await PcsData.updateOne(
+//         { pcNumber : pcNumber, labNumber : labNumber, macAddress : macAddress, }, 
+//         { $set : clintdata }
+//     );
+//     res.send('done')
+// }
+
+// const getAllInfo = async (req, res) => {
+//     // mongodb
+//     const data = await PcsData.find()
+    
+//     // res.send('getAllInfo is working')
+//     res.json(data)
+//     // sind all info in database
+// }
+
+// const shutdownAPC =  (req, res) => {
+//     const lab = req.params.lab
+//     const pcId = req.params.pcId
+
+//     // numbers or "all"
+//     // res.json()
+//                                                         // do the shut down
+//                                                         shutdownWS.shutdownIt(lab, pcId)
+//     // WS
+//     res.send('shutdownAPC is working')
+// }
+
+// const shutdownAllowence = async (req, res) => {
+//     const clint = req.body
+//     const {pcNumber, labNumber, macAddress} = req.body
+//     // is it allowed to do so??
+//     // res.send({states : true})
+//     // mongodb
+//     // res.send({states : true, time : "2024-07-23 08:00:00"})
+
+//     const now = new Date.now();
+//     // console.log(now); 
+
+//     const clintInDB = await PcsData.findOne(
+//         { pcNumber : pcNumber, labNumber : labNumber, macAddress : macAddress, }
+//     );
+
+//     if (clintInDB.openTimeAllowed < now || clintInDB.openTimeAllowed == null) {
+
+//                                                         res.send({states : true})
+//                                                         // you may do the shut down
+//                                                         // res.send(true)
+//     }
+//     else {
+//                                                         res.send({states : false, time : (clintInDB.openTimeAllowed - now)})
+//     }
+
+
+
+//     // res.send('shutdownAllowence is working')
+
+// }
+
+// const clintAllowanceChange = async (req, res) => {
+//     // the pc and the time
+//     const clint = req.body
+//     // mongodb
+//     // res.send('done')
+
+//     const {pcNumber, labNumber, openTimeAllowed} = req.body
+//     // res.json(clint)
+//     // res.send('updateDatabaseClint is working')
+//     // const newPcsData = new PcsData(clintdata)
+//     // mongodb
+//     // update database with (clint)
+//     // await newPcsData.save()
+//     await PcsData.updateOne(
+//         { pcNumber : pcNumber, labNumber : labNumber }, 
+//         { openTimeAllowed : openTimeAllowed }
+//     );
+// // {
+// //     "pcNumber" : 1,
+// //     "labNumber" : 1,
+// //     "openTimeAllowed" : "2026-04-25 20:26:50"
+// // }
+//     res.send('done')
+
+// }
+
+// const getdashBoard =  (req, res) => {
+//     // will be reblaced with github pages
+//     res.send('getdashBoard is working')
+// }
+
+// module.exports = {
+//     starting,
+//     updateDatabaseClint,
+//     getAllInfo,
+//     shutdownAPC,
+//     shutdownAllowence,
+//     clintAllowanceChange,
+//     getdashBoard,
+//     addToDatabaseClint
+// }
+
+
+
+
+
+
+
+
+
+// =============================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PcsData = require('../models/lab_controller.model');
+
+let io; // تعريف السوكيت في نطاق الملف
+
+const setSocketIo = (socketIoInstance) => {
+    io = socketIoInstance;
+};
 
 const starting = (req, res) => {
-    res.send('starting is working')
-}
+    res.send('starting is working');
+};
 
 const addToDatabaseClint = async (req, res) => {
-    const clintdata = req.body
-    const {pcNumber, labNumber, macAddress, password} = req.body
-    // res.json(clint)
-    // res.send('updateDatabaseClint is working')
+    const clintdata = req.body;
+    const { password } = req.body;
+    
     if (password == 123) {
-
-        const newPcsData = new PcsData(clintdata.data)
-        
-        await newPcsData.save()
-        res.send('done')
-            // {
-            //     "password" : 123,   
-            //     "data" : {
-            //         "status": {
-            //             "mouce": "2023-10-25T10:30:00.000Z",
-            //             "keyboard": "2023-10-25T10:30:00.000Z",
-            //             "openedTab": ["goos.com"]
-            //         },
-            //         "macAddress": "02:1A:2B:dsfsdfdfsdf:4D:5E",
-            //         "pcNumber": 18,
-            //         "labNumber": 1,
-            //         "timeOfBoot": "2023-10-25T08:00:00.000Z",
-            //         "openTimeAllowed": null
-            //     }
-            // }
+        try {
+            const newPcsData = new PcsData(clintdata.data);
+            await newPcsData.save();
+            res.send('done');
+        } catch (err) {
+            res.status(500).send('Error saving to DB');
+        }
     } else {
-        res.send('error')
+        res.send('error');
     }
-    // mongodb
-    // update database with (clint)
-
-    // await PcsData.updateOne(
-    //     { pcNumber : pcNumber, labNumber : labNumber, macAddress : macAddress, }, 
-    //     { $set : clintdata }
-    // );
-}
+};
 
 const updateDatabaseClint = async (req, res) => {
-    const clintdata = req.body
-    const {pcNumber, labNumber, macAddress} = req.body
-    // res.json(clint)
-    // res.send('updateDatabaseClint is working')
-    // const newPcsData = new PcsData(clintdata)
-    // mongodb
-    // update database with (clint)
-    // await newPcsData.save()
-    await PcsData.updateOne(
-        { pcNumber : pcNumber, labNumber : labNumber, macAddress : macAddress, }, 
-        { $set : clintdata }
-    );
-    res.send('done')
-}
+    const clintdata = req.body;
+    const { pcNumber, labNumber, macAddress } = req.body;
+    try {
+        await PcsData.updateOne(
+            { pcNumber, labNumber, macAddress }, 
+            { $set: clintdata }
+        );
+        res.send('done');
+    } catch (err) {
+        res.status(500).send('Error updating DB');
+    }
+};
 
 const getAllInfo = async (req, res) => {
-    // mongodb
-    const data = await PcsData.find()
-    
-    // res.send('getAllInfo is working')
-    res.json(data)
-    // sind all info in database
-}
+    const data = await PcsData.find();
+    res.json(data);
+};
 
-const shutdownAPC =  (req, res) => {
-    const lab = req.params.lab
-    const pcId = req.params.pcId
+const shutdownAPC = (req, res) => {
+    const lab = req.params.lab;
+    const pcId = req.params.pcId;
 
-    // numbers or "all"
-    // res.json()
-                                                        // do the shut down
-                                                        shutdownWS.shutdownIt(lab, pcId)
-    // WS
-    res.send('shutdownAPC is working')
-}
+    if (io) {
+        if (pcId === 'all') {
+            io.to(`lab_${lab}`).emit('force_shutdown', { message: 'Server forced shutdown' });
+        } else {
+            io.to(`lab_${lab}_pc_${pcId}`).emit('force_shutdown', { message: 'Server forced shutdown' });
+        }
+        res.send('shutdown command sent');
+    } else {
+        res.status(500).send('Socket.io not initialized');
+    }
+};
 
 const shutdownAllowence = async (req, res) => {
-    const clint = req.body
-    const {pcNumber, labNumber, macAddress} = req.body
-    // is it allowed to do so??
-    // res.send({states : true})
-    // mongodb
-    // res.send({states : true, time : "2024-07-23 08:00:00"})
+    const { pcNumber, labNumber, macAddress } = req.body;
+    const now = new Date();
 
-    const now = new Date.now();
-    // console.log(now); 
+    try {
+        const clintInDB = await PcsData.findOne({ pcNumber, labNumber, macAddress });
 
-    const clintInDB = await PcsData.findOne(
-        { pcNumber : pcNumber, labNumber : labNumber, macAddress : macAddress, }
-    );
+        if (!clintInDB) {
+            return res.status(404).send({ error: 'PC not found' });
+        }
 
-    if (clintInDB.openTimeAllowed < now || clintInDB.openTimeAllowed == null) {
-
-                                                        res.send({states : true})
-                                                        // you may do the shut down
-                                                        // res.send(true)
+        if (!clintInDB.openTimeAllowed || clintInDB.openTimeAllowed < now) {
+            res.send({ states: true });
+        } else {
+            res.send({ states: false, timeRemaining: (clintInDB.openTimeAllowed - now) });
+        }
+    } catch (err) {
+        res.status(500).send('Internal Server Error');
     }
-    else {
-                                                        res.send({states : false, time : (clintInDB.openTimeAllowed - now)})
-    }
-
-
-
-    // res.send('shutdownAllowence is working')
-
-}
+};
 
 const clintAllowanceChange = async (req, res) => {
-    // the pc and the time
-    const clint = req.body
-    // mongodb
-    // res.send('done')
+    const { pcNumber, labNumber, openTimeAllowed } = req.body;
+    try {
+        await PcsData.updateOne(
+            { pcNumber, labNumber }, 
+            { openTimeAllowed: new Date(openTimeAllowed) }
+        );
+        res.send('done');
+    } catch (err) {
+        res.status(500).send('Error updating allowance');
+    }
+};
 
-    const {pcNumber, labNumber, openTimeAllowed} = req.body
-    // res.json(clint)
-    // res.send('updateDatabaseClint is working')
-    // const newPcsData = new PcsData(clintdata)
-    // mongodb
-    // update database with (clint)
-    // await newPcsData.save()
-    await PcsData.updateOne(
-        { pcNumber : pcNumber, labNumber : labNumber }, 
-        { openTimeAllowed : openTimeAllowed }
-    );
-// {
-//     "pcNumber" : 1,
-//     "labNumber" : 1,
-//     "openTimeAllowed" : "2026-04-25 20:26:50"
-// }
-    res.send('done')
+const getdashBoard = (req, res) => {
+    res.send('getdashBoard is working');
+};
 
-}
-
-const getdashBoard =  (req, res) => {
-    // will be reblaced with github pages
-    res.send('getdashBoard is working')
-}
-
+// التأكد من تصدير كل الدوال بشكل صحيح
 module.exports = {
+    setSocketIo,
     starting,
     updateDatabaseClint,
     getAllInfo,
@@ -153,4 +290,4 @@ module.exports = {
     clintAllowanceChange,
     getdashBoard,
     addToDatabaseClint
-}
+};
--- index.js
+++ index.js
@@ -1,38 +1,79 @@
-const express = require('express')
-const app = express()
-const port = 6000
+// const express = require('express')
+// const app = express()
+// const port = 6000
 
-const mongoose = require('mongoose')
-const DBurl = 'mongodb+srv://omarrashed1000:bodo@learn-mango-db.ixz7ecc.mongodb.net/lab_controller_DB?appName=learn-mango-db'
-mongoose.connect(DBurl).then(()=>{
-    console.log('connected to db');
-})
+// const mongoose = require('mongoose')
+// const DBurl = 'mongodb+srv://omarrashed1000:bodo@learn-mango-db.ixz7ecc.mongodb.net/lab_controller_DB?appName=learn-mango-db'
+// mongoose.connect(DBurl).then(()=>{
+//     console.log('connected to db');
+// })
 
-app.use(express.json())
+// app.use(express.json())
 
-const lab_router = require('./routes/routes.lab_controller')
+// const lab_router = require('./routes/routes.lab_controller')
 
-app.use('/', lab_router)
-
-
+// app.use('/', lab_router)
 
 
 
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`app listening on port ${port}`)
+// })
+
+
+// =============================================================================================================
 
 
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`app listening on port ${port}`)
-})
+
+const express = require('express');
+const http = require('http'); // بنحتاجه عشان الـ WebSocket
+const { Server } = require('socket.io');
+const mongoose = require('mongoose');
+
+const app = express();
+const server = http.createServer(app); // إنشاء سيرفر HTTP
+const io = new Server(server, {
+    cors: { origin: "*" } // عشان يسمح بالاتصال من أي مكان حالياً
+});
+
+const port = 6000;
+
+// المانجو دي بي
+const DBurl = 'mongodb+srv://omarrashed1000:bodo@learn-mango-db.ixz7ecc.mongodb.net/lab_controller_DB?appName=learn-mango-db';
+mongoose.connect(DBurl)
+    .then(() => console.log('connected to db'))
+    .catch((err) => {
+        console.error('MongoDB connection error:', err.message);
+        // كده السيرفر هيفضل شغال حتى لو الداتا بيز وقعت، وهيديك رسالة واضحة
+    });
 
 
-// const now = new Date();
-// const options = {
-//   year: 'numeric', month: '2-digit', day: '2-digit',
-//   hour: '2-digit', minute: '2-digit', second: '2-digit',
-//   hour12: false
-// };
-// const formatter = new Intl.DateTimeFormat('en-CA', options); // en-CA uses YYYY-MM-DD
-// const formatted = formatter.format(now).replace(', ', ' ');
-// // Result: "2024-07-23 08:00:00"
-// console.log(formatted)
+app.use(express.json());
+
+// هنمرر الـ io للـ controller عشان يقدر يبعت رسايل
+const lab_controller = require('./controllers/controllers.lab_controller');
+lab_controller.setSocketIo(io); 
+
+const lab_router = require('./routes/routes.lab_controller');
+app.use('/', lab_router);
+
+// إدارة اتصالات الـ WebSocket
+io.on('connection', (socket) => {
+    console.log('A client connected:', socket.id);
+    
+    // الكلاينت لما يفتح يسجل نفسه بغرفة (Room) برقم الجهاز والسكشن
+    socket.on('register', (data) => {
+        const roomName = `lab_${data.labNumber}_pc_${data.pcNumber}`;
+        socket.join(roomName);
+        console.log(`PC ${data.pcNumber} in Lab ${data.labNumber} joined room ${roomName}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening on port ${port}`);
+});
